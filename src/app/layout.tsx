2e4842d7bb3391019fee3e274ac7dd1d5aca4b90
m4cone <boffmarcone@gmail.com>
2025-08-24 13:08:01 -0300

    feat: disable zoom in mobile screen when input focus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
       </head>
       <body>{children}</body>
     </html>
